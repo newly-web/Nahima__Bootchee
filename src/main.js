2051bbe49f9672de6a06a2657be7989811039cdc
Whitney Nsegbe <whitneynsegbe@gmail.com>
2025-06-05 18:55:11 -0400

    cursor and style changes, finishing touches

--- src/main.js
+++ src/main.js
@@ -3,3 +3,16 @@ window.addEventListener("load", function () {
     const loader = document.getElementById("preloader");
     loader.classList.add("fade-out"); // Fade out loader
 });
+
+// automatic email redirect
+window.handleEmailClick = function () {
+    const email = "Jaciliennahimabusiness@gmail.com";
+   
+    navigator.clipboard.writeText(email)
+        .then(() => alert("Email copied to clipboard!"))
+        .catch(err => {
+            alert("Failed to copy email.");
+            console.error(err);
+        });
+};
+
